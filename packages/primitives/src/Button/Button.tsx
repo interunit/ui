f8b1f35db3b3134d6f57eb84f24b693c7013d8a4
Kyle McDonald <hello@kylemcd.com>
2023-06-16 14:30:19 -0500

    Button Accessibility

--- packages/primitives/src/Button/Button.tsx
+++ packages/primitives/src/Button/Button.tsx
@@ -17,14 +17,47 @@ type ButtonPrimitiveProps = ButtonElementProps & {
   as?: ButtonElementAs | AnyComponent
   type?: 'button' | 'submit' | 'reset'
   role?: 'button' | 'link'
+  disabled?: boolean
   children: React.ReactNode
+
+  /*
+   * Similar accessibility props between React Native and Web
+   */
+
+  // Web Accessibility
+  'aria-label'?: string
+
+  // React Native Accessibility
+  accessible?: boolean
+  accessibilityLabel?: string
+  accessibilityRole?: string
+  accessibilityState?: {
+    disabled?: boolean
+  }
 }
 
 const Button = React.forwardRef<ButtonElementComponent, ButtonPrimitiveProps>(
   (
-    {type = 'button', role = 'button', as = 'button', children, ...props},
+    {
+      as = 'button',
+      type = 'button',
+      role = 'button',
+      disabled,
+      children,
+      ...props
+    },
     forwardedRef
   ) => {
+    /*
+     * Map similar accessibility props between React Native and Web
+     */
+    const accessibilityProps = {
+      accessible: props.accessible ?? true,
+      accessibilityLabel: props.accessibilityLabel ?? props['aria-label'],
+      accessibilityRole: props.accessibilityRole ?? role,
+      accessibilityState: props.accessibilityState ?? {disabled}
+    }
+
     const Button = getElementFromAs<typeof ButtonElement, ButtonPrimitiveProps>(
       {
         as: as,
@@ -34,10 +67,12 @@ const Button = React.forwardRef<ButtonElementComponent, ButtonPrimitiveProps>(
 
     return (
       <Button
+        {...addOptionalProp(as)}
         type={type}
         role={role}
+        disabled={disabled}
         ref={forwardedRef}
-        {...addOptionalProp(as)}
+        {...accessibilityProps}
         {...props}
       >
         {children}
