c69f258a222b1d179d0d3099c943ebe597207712
Kyle McDonald <hello@kylemcd.com>
2023-07-24 22:56:20 -0500

    🐛 FIX: Correct syntax for ts ignore

--- apps/interunit-dot-dev/next.config.js
+++ apps/interunit-dot-dev/next.config.js
@@ -4,11 +4,13 @@
 const withPreconstruct = require('@preconstruct/next')
 
 const nextConfig = {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    }
 }
 
 module.exports = withPreconstruct({...nextConfig})
