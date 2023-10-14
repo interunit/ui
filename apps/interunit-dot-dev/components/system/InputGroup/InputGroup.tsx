bdb35c9c5448805e25e5becec33e1f8232899598
Kyle McDonald <hello@kylemcd.com>
2023-10-14 10:06:09 -0500

    🐛 FIX: Linting error

--- apps/interunit-dot-dev/components/system/InputGroup/InputGroup.tsx
+++ apps/interunit-dot-dev/components/system/InputGroup/InputGroup.tsx
@@ -1,14 +1,17 @@
-import {Div, Primitive} from '@interunit/primitives'
+import type { Primitive} from '@interunit/primitives';
+import {Div} from '@interunit/primitives'
 import React from 'react'
 
-type InputGroupProps = React.ComponentPropsWithoutRef<typeof Primitive.Box<'div'>> & {
+type InputGroupProps = React.ComponentPropsWithoutRef<
+  typeof Primitive.Box<'div'>
+> & {
   children?: React.ReactNode
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ref?: React.Ref<any>
 }
 
 const InputGroup = React.forwardRef<any, InputGroupProps>(
-  ({ className, children, ...props}, forwardedRef) => {
+  ({className, children, ...props}, forwardedRef) => {
     return (
       <Div
         className={`mb-4 full-width ${className}`}
