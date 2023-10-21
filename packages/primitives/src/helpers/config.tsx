794fd02ff0bed1614925418b62b8a53f4c48e276
Kyle McDonald <hello@kylemcd.com>
2023-10-20 22:28:09 -0500

    fix missing process catch

--- packages/primitives/src/helpers/config.tsx
+++ packages/primitives/src/helpers/config.tsx
@@ -9,9 +9,30 @@ import type React from 'react'
 
 import {getConstruct} from './getConstruct'
 
-const ENVIRONMENT =
-  process.env.TEST_ENV_NAME ??
-  InterUnitInternals.InterUnitInternalConfig.ENVIRONMENT.NAME
+export const getEnvironmentName = () => {
+  // Jest doesn't seem to crawl to this file correctly
+  // so native tests run in a web env. This will fix that.
+  // Might need to rethink this using haste or some other solution
+  // if more problems arise from this same issue.
+  try {
+    if (
+      process?.env?.TEST_ENV_NAME &&
+      process?.env?.TEST_ENV_NAME !== undefined
+    ) {
+      if (
+        process.env.TEST_ENV_NAME !== ('native' as string) &&
+        process.env.TEST_ENV_NAME !== ('web' as string)
+      ) {
+        throw new Error('TEST_ENV_NAME must be either "native" or "web"')
+      }
+      return process.env.TEST_ENV_NAME
+    }
+    return InterUnitInternals.InterUnitInternalConfig.ENVIRONMENT.NAME
+  } catch (e) {
+    return InterUnitInternals.InterUnitInternalConfig.ENVIRONMENT.NAME
+  }
+}
+const ENVIRONMENT = getEnvironmentName()
 
 type ConstructObject = {
   inter: InterConstruct
